c6fc8533b2e584a54467b0d1954e4219309817ce
fran <dominguezfran07@gmail.com>
2026-05-07 18:54:03 -0300

    primer commit

---
+++ DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/DentalOfficeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.utn.DentalOfficeManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalOfficeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalOfficeManagementApplication.class, args);
+	}
+
+}
---
+++ DentalOfficeManagement/src/test/java/com/utn/DentalOfficeManagement/DentalOfficeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.DentalOfficeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentalOfficeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
