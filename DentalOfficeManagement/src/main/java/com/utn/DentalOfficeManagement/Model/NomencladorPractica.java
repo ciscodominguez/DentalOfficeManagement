3aeadfac1621c02d03f194b8c14bcd85250f4806
giorg <giorgionapoli458@gmail.com>
2026-05-18 18:56:38 -0300

    Codigo de primeras 5 entidades con jpa

--- DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Model/NomencladorPractica.java
+++ DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Model/NomencladorPractica.java
@@ -1,4 +1,28 @@
 package com.utn.DentalOfficeManagement.Model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "nomenclador_practica")
+@Data
+@NoArgsConstructor
 public class NomencladorPractica {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_nomenclador_practica")
+    private Integer idNomencladorPractica;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_obra_social", nullable = false)
+    private ObraSocial obraSocial;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "practica_id", nullable = false)
+    private Practica practica;
+
+    @Column(name = "codigo_externo", nullable = false, length = 50)
+    private String codigoExterno;
 }
