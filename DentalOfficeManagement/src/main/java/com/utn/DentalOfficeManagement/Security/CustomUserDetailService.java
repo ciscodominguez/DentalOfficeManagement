c1254f013bf0cceea522cd69f360f1e1f2d8f625
giorg <giorgionapoli458@gmail.com>
2026-06-03 23:51:37 -0300

    Se crearon los paquetes y clases respectivos a security

---
+++ DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Security/CustomUserDetailService.java
@@ -0,0 +1,25 @@
+package com.utn.DentalOfficeManagement.Security;
+
+import com.utn.DentalOfficeManagement.Model.Usuario;
+import com.utn.DentalOfficeManagement.Repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Usuario usuario = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado con email: " + email));
+        return new CustomUserDetails(usuario);
+    }
+}
---
+++ DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Security/CustomUserDetails.java
@@ -0,0 +1,55 @@
+package com.utn.DentalOfficeManagement.Security;
+
+import com.utn.DentalOfficeManagement.Model.Usuario;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final Usuario usuario;
+
+    public CustomUserDetails(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return usuario.getContrasenia();
+    }
+
+    @Override
+    public String getUsername() {
+        return usuario.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
+
+
