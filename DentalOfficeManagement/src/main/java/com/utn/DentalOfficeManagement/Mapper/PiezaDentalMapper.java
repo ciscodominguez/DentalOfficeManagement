7df8c06d28f4461188dfbd8865d785db3356d94b
fran <dominguezfran07@gmail.com>
2026-06-17 13:10:26 -0300

    arreglos para correcto mapeo

--- DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Mapper/PiezaDentalMapper.java
+++ DentalOfficeManagement/src/main/java/com/utn/DentalOfficeManagement/Mapper/PiezaDentalMapper.java
@@ -4,6 +4,7 @@ import com.utn.DentalOfficeManagement.DTO.Request.PiezaDentalRequestDTO;
 import com.utn.DentalOfficeManagement.DTO.Response.PiezaDentalResponseDTO;
 import com.utn.DentalOfficeManagement.Model.PiezaDental;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
@@ -11,6 +12,9 @@ public interface PiezaDentalMapper {
 
     PiezaDental requestDtoToEntity(PiezaDentalRequestDTO dto);
 
+    @Mapping(target = "numero", source = "numeroPieza")
+    @Mapping(target = "nombre", source = "estado")
+    @Mapping(target = "posicion", source = "observacion")
     PiezaDentalResponseDTO entityToResponseDto(PiezaDental entity);
 
     void updateEntityFromRequestDto(PiezaDentalRequestDTO dto, @MappingTarget PiezaDental entity);
